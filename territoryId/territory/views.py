425dcdac647a19a2d8013d94614e5b29bcff44c7
FlubberChiflado <flubberchiflado@gmail.com>
2023-11-25 17:41:58 +0100

    Upgrade user register

--- territoryId/territory/views.py
+++ territoryId/territory/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
-
-# Create your views here.
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
 from .facenet import FaceRecognition
@@ -18,3 +17,17 @@ def login_view(request):
 
 def sign_up_view(request):
     return render(request, 'sign_up.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Cuenta creada para {username}!')
+            login(request, user)
+            return redirect('some-view')
+    else:
+        form = UserCreationForm()
+    return render(request, 'sign_up.html', {'form': form})
+
